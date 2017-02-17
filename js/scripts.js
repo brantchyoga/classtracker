e51d6c0b2cc9ac26d1a1ad5c28552a6731297180
Brant Porter <brantrporter@gmail.com>
2017-02-17 14:18:07 -0800

    Shows all class tracks with input of info using if and else statements

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@
 $(function() {
   $("form#basic").submit(function(event) {
     var name = $("input#name").val();
-    var city = toLowerCase($("input#city").val());
+    var city = ($("input#city").val()).toLowerCase();
     var date = $("select#rush").val();
     var color = $("input:radio[name=color]:checked").val();
     var hobbie = $("select#hobbie").val();
@@ -24,9 +24,20 @@ $(function() {
     if (hobbie === "paint") {
       $("#inputclasstrack").text("CSS/Design");
       $("#hidden").show();
-    } else if (()) {
-
+    } else if (color === "black") {
+      $("#inputclasstrack").text("C#/.NET");
+      $("#hidden").show();
+    } else if (color === "blue") {
+      $("#inputclasstrack").text("PHP/Drupal");
+      $("#hidden").show();
+    } else if (color === "red") {
+      $("#inputclasstrack").text("Ruby/Rails");
+      $("#hidden").show();
+    } else if (color === "green") {
+      $("#inputclasstrack").text("Java/Android");
+      $("#hidden").show();
+    } else {
+      alert("Make sure all fields of the survey are filled out correctly!!!")
     }
-
   });
 });
