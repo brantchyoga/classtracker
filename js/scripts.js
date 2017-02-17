0d1d2093630167954e683e3321f111020290e80d
Brant Porter <brantrporter@gmail.com>
2017-02-17 13:43:29 -0800

    input first class track with if statement

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,23 @@
 $(function() {
   $("form#basic").submit(function(event) {
     var name = $("input#name").val();
-    var city = $("input#city").val();
+    var city = toLowerCase($("input#city").val());
     var date = $("select#rush").val();
+    var color = $("input:radio[name=color]:checked").val();
+    var hobbie = $("select#hobbie").val();
+    console.log(name);
+    console.log(city);
+    console.log(date);
+    console.log(color);
+    console.log(hobbie);
+    event.preventDefault();
+    $("#inputname").text(name);
+    if (hobbie === "paint") {
+      $("#inputclasstrack").text("CSS/Design");
+      $("#hidden").show();
+    } else if (()) {
+
+    }
+
   });
 });
